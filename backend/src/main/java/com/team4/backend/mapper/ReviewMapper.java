409f09bc43757a64db7c671c4ba7126a10b836d3
unknown <tajphutela@gmail.com>
2025-06-06 14:36:48 +0530

    Added ReviewMapper

--- backend/src/main/java/com/team4/backend/mapper/ReviewMapper.java
+++ backend/src/main/java/com/team4/backend/mapper/ReviewMapper.java
@@ -1,4 +1,15 @@
-package com.team4.backend.entities;
+package com.team4.backend.mapper;
 
+import com.team4.backend.entities.Review;
+import com.team4.backend.dto.ReviewDto;
+import org.mapstruct.*;
+
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING)
 public interface ReviewMapper {
-  }
+    Review toEntity(ReviewDto reviewDto);
+
+    ReviewDto toDto(Review review);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    Review partialUpdate(ReviewDto reviewDto, @MappingTarget Review review);
+}
