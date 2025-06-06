0556fe745db6b65c85e0fad4e794a062b05bd0a7
Nazim Ahmad <nazimahmedchd@gmail.com>
2025-06-06 15:03:53 +0530

    FEAT {route} mapper is created

--- backend/src/main/java/com/team4/backend/mapper/RouteMapper.java
+++ backend/src/main/java/com/team4/backend/mapper/RouteMapper.java
@@ -1,4 +1,15 @@
-package com.team4.backend.entities;
+package com.team4.backend.mapper;
 
+import com.team4.backend.entities.Route;
+import com.team4.backend.dto.RouteDto;
+import org.mapstruct.*;
+
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING)
 public interface RouteMapper {
-  }
+    Route toEntity(RouteDto routeDto);
+
+    RouteDto toDto(Route route);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    Route partialUpdate(RouteDto routeDto, @MappingTarget Route route);
+}
