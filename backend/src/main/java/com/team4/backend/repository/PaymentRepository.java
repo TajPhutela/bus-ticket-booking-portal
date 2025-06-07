df586821508e2fa5fe30fbdc02554ef99331729b
Rithik1157 <rithik1157.be21@chitkarauniversity.edu.in>
2025-06-07 10:30:52 +0530

    Payment Repository Created

--- backend/src/main/java/com/team4/backend/repository/PaymentRepository.java
+++ backend/src/main/java/com/team4/backend/repository/PaymentRepository.java
@@ -1,5 +1,6 @@
 package com.team4.backend.repository;
 
+import com.team4.backend.entities.Booking;
 import com.team4.backend.entities.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,5 +9,7 @@ import java.util.List;
 
 @Repository
 public interface PaymentRepository extends JpaRepository<Payment, Integer> {
+    List<Payment> findByCustomerId(Integer customerId);
+    List<Payment> findByBookingId(Integer bookingId);
 
 }
