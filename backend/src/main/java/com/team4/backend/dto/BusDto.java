c40ab6b099941503e628306e8d3f45b9c7c59255
Anmol Dhiman <73706789+AnmolDh@users.noreply.github.com>
2025-06-08 16:33:46 +0530

    feat: bus mapper helper

--- backend/src/main/java/com/team4/backend/dto/BusDto.java
+++ backend/src/main/java/com/team4/backend/dto/BusDto.java
@@ -9,6 +9,10 @@ import java.io.Serializable;
 /**
  * DTO for {@link Bus}
  */
-public record BusDto(Integer id, @NotNull @Size(max = 20) String registrationNumber, @NotNull Integer capacity,
-                     @NotNull @Size(max = 30) String type) implements Serializable {
-}
+public record BusDto(
+        Integer id,
+        @NotNull @Size(max = 20) String registrationNumber,
+        @NotNull Integer capacity,
+        @NotNull @Size(max = 30) String type,
+        Integer officeId
+) implements Serializable {}
