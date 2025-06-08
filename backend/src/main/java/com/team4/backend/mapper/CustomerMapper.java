0e60e4e3299f3a25d6cf13a3cfe902b118831c62
Anmol Dhiman <73706789+AnmolDh@users.noreply.github.com>
2025-06-08 16:29:43 +0530

    feat: customer mapper helper

--- backend/src/main/java/com/team4/backend/mapper/CustomerMapper.java
+++ backend/src/main/java/com/team4/backend/mapper/CustomerMapper.java
@@ -2,14 +2,23 @@ package com.team4.backend.mapper;
 
 import com.team4.backend.entities.Customer;
 import com.team4.backend.dto.CustomerDto;
+import com.team4.backend.mapper.helper.CustomerMapperHelper;
 import org.mapstruct.*;
 
-@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING)
+@Mapper(
+        componentModel = MappingConstants.ComponentModel.SPRING,
+        uses = CustomerMapperHelper.class,
+        unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
 public interface CustomerMapper {
+
+//    @Mapping(source = "addressId", target = "address", qualifiedByName = "addressFromId")
     Customer toEntity(CustomerDto customerDto);
 
+//    @Mapping(source = "address.id", target = "addressId")
     CustomerDto toDto(Customer customer);
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+//    @Mapping(source = "addressId", target = "address", qualifiedByName = "addressFromId")
     Customer partialUpdate(CustomerDto customerDto, @MappingTarget Customer customer);
 }
