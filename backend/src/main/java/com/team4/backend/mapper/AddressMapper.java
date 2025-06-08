a2323b6ceafb1a7244ac0ec271f161df20b88ca7
Rithik1157 <rithik1157.be21@chitkarauniversity.edu.in>
2025-06-08 23:45:17 +0530

    AddressMapper Created

--- backend/src/main/java/com/team4/backend/mapper/AddressMapper.java
+++ backend/src/main/java/com/team4/backend/mapper/AddressMapper.java
@@ -1,4 +1,15 @@
-package com.team4.backend.entities;
+package com.team4.backend.mapper;
 
+import com.team4.backend.dto.AddressDto;
+import com.team4.backend.entities.Address;
+import org.mapstruct.*;
+
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING)
 public interface AddressMapper {
-  }
+    Address toEntity(AddressDto addressDto);
+
+    AddressDto toDto(Address address);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    Address partialUpdate(AddressDto addressDto, @MappingTarget Address address);
+}
