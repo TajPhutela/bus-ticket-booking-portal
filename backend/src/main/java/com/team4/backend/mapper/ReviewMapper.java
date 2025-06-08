04a1c5a78251c9e3f897cbc2923d6cd5ad7ebc50
Anmol Dhiman <73706789+AnmolDh@users.noreply.github.com>
2025-06-08 16:22:42 +0530

    feat: reviews mapper helper

--- backend/src/main/java/com/team4/backend/mapper/ReviewMapper.java
+++ backend/src/main/java/com/team4/backend/mapper/ReviewMapper.java
@@ -2,14 +2,28 @@ package com.team4.backend.mapper;
 
 import com.team4.backend.entities.Review;
 import com.team4.backend.dto.ReviewDto;
+import com.team4.backend.mapper.helper.ReviewMapperHelper;
 import org.mapstruct.*;
 
-@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING)
+import org.mapstruct.*;
+
+@Mapper(
+        componentModel = MappingConstants.ComponentModel.SPRING,
+        uses = ReviewMapperHelper.class,
+        unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
 public interface ReviewMapper {
+
+    @Mapping(source = "customerId", target = "customer", qualifiedByName = "customerFromId")
+    @Mapping(source = "tripId", target = "trip", qualifiedByName = "tripFromId")
     Review toEntity(ReviewDto reviewDto);
 
+    @Mapping(source = "customer.id", target = "customerId")
+    @Mapping(source = "trip.id", target = "tripId")
     ReviewDto toDto(Review review);
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(source = "customerId", target = "customer", qualifiedByName = "customerFromId")
+    @Mapping(source = "tripId", target = "trip", qualifiedByName = "tripFromId")
     Review partialUpdate(ReviewDto reviewDto, @MappingTarget Review review);
 }
