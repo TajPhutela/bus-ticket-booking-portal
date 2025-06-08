1b6dd5e7d5383ec9fe8d3da506017fb673e3c82e
Rithik1157 <rithik1157.be21@chitkarauniversity.edu.in>
2025-06-08 23:45:50 +0530

    Address Repository Created

--- backend/src/main/java/com/team4/backend/repository/AddressRepository.java
+++ backend/src/main/java/com/team4/backend/repository/AddressRepository.java
@@ -1,4 +1,15 @@
 package com.team4.backend.repository;
 
-public interface AddressRepository {
+import com.team4.backend.entities.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface AddressRepository extends JpaRepository<Address, Integer> {
+
+    List<Address> findByCity(String city);
+
+    List<Address> findByZipCode(String zipCode);
+
+    List<Address> findByState(String phoneNumber);
 }
