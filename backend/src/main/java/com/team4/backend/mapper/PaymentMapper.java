e279fd68eeb2f0ab5f508316324546b7fb79621e
Anmol Dhiman <73706789+AnmolDh@users.noreply.github.com>
2025-06-08 16:26:38 +0530

    feat: payment mapper helper

--- backend/src/main/java/com/team4/backend/mapper/PaymentMapper.java
+++ backend/src/main/java/com/team4/backend/mapper/PaymentMapper.java
@@ -2,12 +2,28 @@ package com.team4.backend.mapper;
 
 import com.team4.backend.entities.Payment;
 import com.team4.backend.dto.PaymentDto;
+import com.team4.backend.mapper.helper.PaymentMapperHelper;
 import org.mapstruct.*;
 
-@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING)public interface PaymentMapper {
+import org.mapstruct.*;
+
+@Mapper(
+        componentModel = MappingConstants.ComponentModel.SPRING,
+        uses = PaymentMapperHelper.class,
+        unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
+public interface PaymentMapper {
+
+//    @Mapping(source = "bookingId", target = "booking", qualifiedByName = "bookingFromId")
+    @Mapping(source = "customerId", target = "customer", qualifiedByName = "customerFromId")
     Payment toEntity(PaymentDto paymentDto);
 
+//    @Mapping(source = "booking.id", target = "bookingId")
+    @Mapping(source = "customer.id", target = "customerId")
     PaymentDto toDto(Payment payment);
 
-    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)Payment partialUpdate(PaymentDto paymentDto, @MappingTarget Payment payment);
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+//    @Mapping(source = "bookingId", target = "booking", qualifiedByName = "bookingFromId")
+    @Mapping(source = "customerId", target = "customer", qualifiedByName = "customerFromId")
+    Payment partialUpdate(PaymentDto paymentDto, @MappingTarget Payment payment);
 }
