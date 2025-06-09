da7d1e00815fb8c33da170e7f887bb881da4f7cf
unknown <tajphutela@gmail.com>
2025-06-09 11:55:55 +0530

    updated BookingMapper

--- backend/src/main/java/com/team4/backend/mapper/BookingMapper.java
+++ backend/src/main/java/com/team4/backend/mapper/BookingMapper.java
@@ -2,14 +2,23 @@ package com.team4.backend.mapper;
 
 import com.team4.backend.dto.BookingDto;
 import com.team4.backend.entities.Booking;
+import com.team4.backend.mapper.helper.BookingMapperHelper;
 import org.mapstruct.*;
 
-@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING, uses = {TripMapper.class})
+@Mapper(
+        componentModel = MappingConstants.ComponentModel.SPRING,
+        uses = BookingMapperHelper.class,
+        unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
 public interface BookingMapper {
+
+    @Mapping(source = "tripId", target = "trip", qualifiedByName = "tripFromId")
     Booking toEntity(BookingDto bookingDto);
 
+    @Mapping(source = "trip.id", target = "tripId")
     BookingDto toDto(Booking booking);
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(source = "tripId", target = "trip", qualifiedByName = "tripFromId")
     Booking partialUpdate(BookingDto bookingDto, @MappingTarget Booking booking);
 }
