974cae211d886aa63ebda805b3b89e555df661dc
Nazim Ahmad <nazimahmedchd@gmail.com>
2025-06-09 12:56:45 +0530

    FEAT {driver} driver repository completed

--- backend/src/main/java/com/team4/backend/repository/DriverRepository.java
+++ backend/src/main/java/com/team4/backend/repository/DriverRepository.java
@@ -1,4 +1,18 @@
 package com.team4.backend.repository;
 
-public interface DriverRepository {
+
+import com.team4.backend.entities.Driver;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface DriverRepository extends JpaRepository<Driver, Integer> {
+
+    List<Driver> findByName(String name);
+
+    List<Driver> findByAddressId(int addressId);
+
+    List<Driver> findByOfficeId(int officeId);
 }
