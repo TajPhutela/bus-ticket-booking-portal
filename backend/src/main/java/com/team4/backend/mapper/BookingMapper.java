904d4798376a2fcdf462117fd4a0eb52b748b688
unknown <tajphutela@gmail.com>
2025-06-09 10:55:43 +0530

    Added booking controller

--- backend/src/main/java/com/team4/backend/mapper/BookingMapper.java
+++ backend/src/main/java/com/team4/backend/mapper/BookingMapper.java
@@ -1,4 +1,15 @@
-package com.team4.backend.entities;
+package com.team4.backend.mapper;
 
+import com.team4.backend.dto.BookingDto;
+import com.team4.backend.entities.Booking;
+import org.mapstruct.*;
+
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING, uses = {TripMapper.class})
 public interface BookingMapper {
-  }
+    Booking toEntity(BookingDto bookingDto);
+
+    BookingDto toDto(Booking booking);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    Booking partialUpdate(BookingDto bookingDto, @MappingTarget Booking booking);
+}
